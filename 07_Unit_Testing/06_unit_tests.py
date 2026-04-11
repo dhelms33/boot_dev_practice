714f8c25ccd608e3ccb6749633e8ccc7e63f4008
dhelms33 <derrickhelms10@gmail.com>
2026-04-10 23:17:19 -0600

    Added unit testing

---
+++ 07_Unit_Testing/06_unit_tests.py
@@ -0,0 +1,9 @@
+def total_xp(level, xp_to_add):
+    total_xp = 0
+    if level == 1 and xp_to_add == 100:
+        total_xp = 200
+    elif level == 2 and xp_to_add == 250:
+        total_xp = 450
+    elif level 170 and xp_to_add == 590:
+        total_xp = 17590
+    return total_xp
