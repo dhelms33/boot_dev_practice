3634c8ee0a5f5924c1b713cc62413d7aaae240b2
dhelms33 <derrickhelms10@gmail.com>
2026-04-13 00:22:14 -0600

    Added unit tests

--- 07_Unit_Testing/06_unit_tests.py
+++ 07_Unit_Testing/06_unit_tests.py
@@ -1,9 +1,14 @@
 def total_xp(level, xp_to_add):
-    total_xp = 0
+    #100 xp per level, convert levels to xp
+    current_xp = level * 100
+    total_xp = xp_to_add + current_xp
+    return total_xp
+
+""" 
     if level == 1 and xp_to_add == 100:
         total_xp = 200
     elif level == 2 and xp_to_add == 250:
         total_xp = 450
-    elif level 170 and xp_to_add == 590:
+    elif level == 170 and xp_to_add == 590:
         total_xp = 17590
-    return total_xp
+"""
